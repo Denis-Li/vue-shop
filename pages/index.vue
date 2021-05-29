83c97bff491250e5b2f203c64390ac0566c0841d
Alex <storyday2012@gmail.com>
2021-05-29 21:45:46 +0200

    test

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,12 @@
 <template>
   <div>
     <h1>Hello Alex</h1>
+    <h3>Test 1 2 3</h3>
   </div>
 </template>
 
 <script>
-export default {}
+export default {};
 </script>
 
-<style>
-
-</style>
+<style></style>
